da52b1c772c298c90edc2a121dd992d2265bf5d2
Justin <justin.koningsberger@gmail.com>
2018-11-07 17:55:58 +0100

    added email scaffold

---
+++ db/migrate/20181107160045_create_emails.rb
@@ -0,0 +1,12 @@
+class CreateEmails < ActiveRecord::Migration[5.1]
+  def change
+    create_table :emails do |t|
+      t.string :sender
+      t.boolean :read
+      t.string :subject
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
