f663fbc0572c1c76a9cc773e1f07e7cc5911a376
Lucas Luitjes <lucas@luitjes.it>
2018-11-09 19:21:04 +0100

    fix incoming_email controller

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -1,7 +1,7 @@
 require 'mail'
 
 class IncomingEmailsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token, only: :create
   def create
     mail = Mail.new(params[:message])
 
