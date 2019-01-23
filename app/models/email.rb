7418e33595d664e9ee57d501905510c28bc4b29f
Justin <justin.koningsberger@gmail.com>
2019-01-23 17:46:55 +0100

    add more email link parsers

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,94 @@
 class Email < ApplicationRecord
+
+  def db_weekly_links
+    html_string = Mail.new(body).html_part.decoded
+    doc = Nokogiri::HTML.parse(html_string)
+    url_elements = doc.search("a")
+    urls = url_elements.map do |n|
+      [
+        n.inner_text.gsub(/\s+/," ").empty?,
+        # filtering by .parent.parent is wonky
+        n.parent.parent.inner_text.gsub(/\s+/," "),
+        /^https:\/\/dbweekly.com\/link\S*/.match(n.attributes["href"].value).to_s
+      ]
+    end
+    urls = urls.select { |link| !link[0] }
+    urls = urls.select { |link| link[2] != "" }
+    # splitting on hardcoded unicode characters is lame
+    urls = urls.map { |link| [link[1].split(" \u2014 "), link[2]].flatten }
+  end
+
+  def java_weekly_links
+    html_string = Mail.new(body).html_part.decoded
+    doc = Nokogiri::HTML.parse(html_string)
+    url_elements = doc.search("a")
+    urls = url_elements.map do |n|
+      [
+        n.parent.parent.inner_text.gsub(/\s+/," "),
+        /^https:\/\/javascriptweekly.com\/link\S*/.match(n.attributes["href"].value).to_s
+      ]
+    end
+    urls = urls.map { |link| [link[0].split(" \u2014 "), link[1]].flatten }
+    # the gsub puts a space that I cant filter out with the line below
+    urls = urls.select { |link| link[2] != "" }
+  end
+
+  def breaking_smart_link
+    # Put single link in array anyway?
+    html_string = Mail.new(body).html_part.decoded
+    doc = Nokogiri::HTML.parse(html_string)
+    url_elements = doc.search("a")
+    url = [url_elements[0].inner_text, url_elements[0].attributes["href"].value]
+  end
+
+  def elixir_weekly_links
+    html_string = Mail.new(body).html_part.decoded
+    doc = Nokogiri::HTML.parse(html_string)
+    url_elements = doc.search("a")
+    url_elements = url_elements.select do |n|
+      url = n.attributes["href"].value
+      url =~ /^https:\/\/elixirweekly.net\/i\//
+    end
+    urls = url_elements.map do |n|
+      [
+        n.parent.children[1].text,
+        n.parent.children[4]&.text || "",
+        n.parent.children[1].attributes["href"].value
+      ]
+    end
+  end
+
+  def hacker_newsletter_links
+    # TODO group links by h2 headers, remove comment links
+    html_string = Mail.new(body).html_part.decoded
+    doc = Nokogiri::HTML.parse(html_string)
+    url_elements = doc.search("a")
+    url_elements = url_elements.select do |n|
+      url = n.attributes["href"].value
+      url =~ /^https:\/\/hackernewsletter.us1.list-manage.com\/track\//
+    end
+    urls = url_elements.map do |n|
+      puts n
+      [
+        n.text,
+        n.attributes["href"].value
+      ]
+    end
+    require 'pry'; binding.pry
+  end
+
+  def blendle_links
+    # work with Lucas
+    html_string = Mail.new(body).html_part.decoded
+    doc = Nokogiri::HTML.parse(html_string)
+    url_elements = doc.search("a")
+    urls = url_elements.map do |n|
+      [
+        n.parent.parent.inner_text.gsub(/\s+/," "),
+        /^https:\/\/javascriptweekly.com\/link\S*/.match(n.attributes["href"].value).to_s
+      ]
+    end
+  end
 end
+
+
--- test/models/email_test.rb
+++ test/models/email_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
+require 'mail'
+require 'pp'
 
 class EmailTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "grab text from db_weekly" do
+  	email = Email.new(body: File.read("test/fixtures/files/dbweekly.txt"))
+    urls = email.db_weekly_links
+    assert_equal ["Redis Speeds Towards a Multi-Model Future",
+ "Redis is best known as a very fast key-value store, but there’s more to it than that especially if Redis Labs gets its way. Here’s an update on where Redis is at right now.",
+ "https://dbweekly.com/link/53785/85a4c1edbc"], urls[1]
+ assert_equal 18, urls.size
+  end
+
+  test "grab text from java_weekly" do
+    email = Email.new(body: File.read("test/fixtures/files/java_weekly.eml"))
+    urls = email.java_weekly_links
+    # assert_equal [], urls[1]
+  end
+  test "grab text from breaking smart" do
+    email = Email.new(body: File.read("test/fixtures/files/breaking_smart.eml"))
+    url = email.breaking_smart_link
+    assert_equal ["View this email in your browser", "https://mailchi.mp/ribbonfarm/good-afternoon-internet-im-listening?e=4c5a41c2ff"], url
+    assert_equal 1, url.size
+  end
+
+  test "grab text from elixir weekly" do
+    email = Email.new(body: File.read("test/fixtures/files/elixir_weekly.eml"))
+    urls = email.elixir_weekly_links
+    assert_equal 22, urls.size
+  end
+
+  test "grab text from hacker newsletter" do
+    email = Email.new(body: File.read("test/fixtures/files/Hacker\ Newsletter\ \#434.eml"))
+    urls = email.hacker_newsletter_links
+  end
+
+  test "grab text from blendle" do
+    email = Email.new(body: File.read("test/fixtures/files/blendle.eml"))
+    urls = email.blendle_links
+  end
 end
