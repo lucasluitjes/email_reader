34e0b1dad5e3f73bfbb387e1700f3ef4ff26ffb1
Lucas Luitjes <lucas@luitjes.it>
2019-07-04 14:02:19 +0200

    basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  if Rails.env == 'production'
+    username = ENV["BASIC_USERNAME"]
+    password = ENV["BASIC_PASSWORD"]
+    raise "Total brutal lack of configuration, security kitty is not pleased :(" unless username.present? && password.present?
+    http_basic_authenticate_with name: username, password: password
+  end
 end
