d5ca0590702666f16eee879d27715c38da90b1e3
Lucas Luitjes <lucas@luitjes.it>
2019-07-04 14:19:54 +0200

    bug

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,13 @@
 class EmailsController < ApplicationController
   before_action :set_email, only: [:show, :edit, :update, :destroy]
 
+  if Rails.env == 'production'
+    username = ENV["BASIC_USERNAME"]
+    password = ENV["BASIC_PASSWORD"]
+    raise "Total brutal lack of configuration, security kitty is not pleased :(" unless username.present? && password.present?
+    http_basic_authenticate_with name: username, password: password
+  end
+  
   # GET /emails
   # GET /emails.json
   def index
