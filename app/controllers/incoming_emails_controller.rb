ac305128d294782b140505eecdaa7d0171aa77f5
Lucas Luitjes <lucas@luitjes.it>
2019-07-04 14:13:35 +0200

    basic athentication

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -1,27 +1,17 @@
 require 'mail'
 
 class IncomingEmailsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token,:basic_authentication, only: :create
   def create
     mail = Mail.new(params[:message])
 
-    doc = Nokogiri::HTML.parse(mail.html_part.body.raw_source) rescue nil
-    if doc&.xpath("html/body/div/text()")&.map(&:text)&.include?("This week's database news")
-      process_dbweekly(doc)
-    end
-
-    Email.create(
+    email = Email.create(
       sender: mail.from,
       subject: mail.subject,
       body: params[:message]
       )
+    email.create_links!
 
     render text: "OK"
-  end
-
-  private
-
-  def process_dbweekly(doc)
-    binding.pry
   end
 end
