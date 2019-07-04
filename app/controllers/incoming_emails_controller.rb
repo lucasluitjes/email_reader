dd26873d198381f8918ef933ad226cfc78ee85dc
Lucas Luitjes <lucas@luitjes.it>
2019-07-04 14:37:25 +0200

    rebug

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -1,7 +1,7 @@
 require 'mail'
 
 class IncomingEmailsController < ApplicationController
-  skip_before_action :verify_authenticity_token,:basic_authentication, only: :create
+  skip_before_action :verify_authenticity_token, only: :create
   def create
     mail = Mail.new(params[:message])
 
