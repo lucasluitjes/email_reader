d4cbcdfff731adce9ee9136f339df40129e81cc4
Lucas Luitjes <lucas@luitjes.it>
2019-09-30 17:03:46 +0200

    Fix another bug

--- public/lazy_loading.js
+++ public/lazy_loading.js
@@ -1,6 +1,6 @@
 document.title = '[' + window.location.hash.substr(1).replace('http://', '').replace('https://', '') + ']';
 
 // load site on focus
-window.addEventListener('focus', function(event) { 
-  window.location = window.location.hash.substr(1);
-}, false);
+document.addEventListener('visibilitychange', function(){
+    window.location = window.location.hash.substr(1);
+})
