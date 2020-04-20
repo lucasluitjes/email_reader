7a1499729ec82bc2275d19a731bead492c743837
Justin <justin.koningsberger@gmail.com>
2020-04-20 17:25:10 +0200

    Hacker newsletter notification

---
+++ app/jobs/send_notification_job..rb
@@ -0,0 +1,9 @@
+class SendNotificationJob < ApplicationJob
+  queue_as :default
+
+  def perform(email)
+    if email.sender == Email::Symbols.invert[:hacker_newsletter]
+      UserMailer.h_newsletter_notification.deliver_now
+    end
+  end
+end
