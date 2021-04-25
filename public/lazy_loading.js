f11807e2e79efd8c4f12fcb2f847482f335fb0ed
Lucas Luitjes <lucashubs@mindrules.net>
2021-04-25 20:14:58 +0200

    Update lazy_loading.js

--- public/lazy_loading.js
+++ public/lazy_loading.js
@@ -1,6 +1,11 @@
 document.title = '[' + window.location.hash.substr(1).replace('http://', '').replace('https://', '') + ']';
 
-// load site on focus
-document.addEventListener('visibilitychange', function(){
+// load site after random delay to avoid rate limiting errors
+setTimeout(function(){ 
     window.location = window.location.hash.substr(1);
-})
+}, Math.floor(Math.random() * 30000));
+
+// load site on focus
+// document.addEventListener('visibilitychange', function(){
+//     window.location = window.location.hash.substr(1);
+// })
